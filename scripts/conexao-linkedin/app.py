bda8b261a642b13057eabcea0c026ab2e9165032
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-03-08 23:06:59 -0300

    linkedin pt 2

--- scripts/conexao-linkedin/app.py
+++ scripts/conexao-linkedin/app.py
@@ -1,5 +1,7 @@
+import keyboard
 import pyautogui as py
 import time
+import clipboard
 
 py.PAUSE = 0.3
 
@@ -14,18 +16,23 @@ py.write("https://www.linkedin.com/home")
 py.press("enter")
 time.sleep(2)
 
-# clica em buscar e digita python (no meu caso estou fazendo uma busca para conexões com pessoas que tbm usam python)
+# clica em buscar no linkedin e digita python (no meu caso estou fazendo uma busca para conexões com pessoas que tbm usam python)
 py.click(x=514, y=149)
-py.write("headhunter")
+py.write("Python")
 py.press("enter")
-time.sleep(5)
+time.sleep(7)
 
-# clica na aba pessoas e depois clica em conectar (tem como configurar para mandar uma mensagem para pessoa)
+# clica na aba pessoas
 py.click(x=435, y=342)
 time.sleep(1)
+
 py.click(x=1130, y=376)
 time.sleep(1)
+py.doubleClick(1085, y=238) # seleciono o nome da pessoa para fazer uma copia
+py.hotkey("ctrl", "c")
+texto = clipboard.paste()
 py.click(x=980, y=344)
-#py.write(" digite uma msg ")
+time.sleep(2)
+keyboard.write(f"Olá {texto},tudo bem? Obrigado pela conexão.") # Digite o texto junto com o nome da pessoa
 time.sleep(1)
 py.click(x=1178, y=526)

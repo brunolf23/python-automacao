1437f77519027d0f9ec3a832bc27ad4fb727c8e3
Bruno <bruno.lf@hotmail.com>
2024-04-12 18:40:53 -0300

    alterando planilha excel v2

---
+++ scripts/alterando-letra-excel/app.py
@@ -0,0 +1,13 @@
+# adicionar uma letra dentro de uma lista
+
+import pyautogui as py
+import time
+
+time.sleep(6)
+
+while True:
+    py.write("@")
+    py.press("space")
+    py.press("down")
+    py.press("home")
+    time.sleep(1)

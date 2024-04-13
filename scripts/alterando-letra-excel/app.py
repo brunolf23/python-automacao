1fba9a28eddf69a7d2d633f393ec6e824c22511b
Bruno <bruno.lf@hotmail.com>
2024-04-13 19:12:47 -0300

    alterando planilha excel v3

--- scripts/alterando-letra-excel/app.py
+++ scripts/alterando-letra-excel/app.py
@@ -6,8 +6,26 @@ import time
 time.sleep(6)
 
 while True:
-    py.write("@")
+    # copiar a coluna desejada e colar
+    py.hotkey("ctrl", "c")
+    time.sleep(1)
+    py.hotkey("alt", "tab")
+    time.sleep(1)
+    py.hotkey("ctrl", "v")
+    py.press("backspace")
     py.press("space")
+    py.write("@")
+    time.sleep(1)
+    # copiar a segunda coluna desejada e colar
+    py.hotkey("alt", "tab")
+    py.press("left")
+    py.hotkey("ctrl", "c")
+    time.sleep(1)
+    py.hotkey("alt", "tab")
+    time.sleep(1)
+    py.hotkey("ctrl", "v")
+    # preparar para a rotação infinita
+    py.hotkey("alt", "tab")
     py.press("down")
-    py.press("home")
+    py.press("right")
     time.sleep(1)
